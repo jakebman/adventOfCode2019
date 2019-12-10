0f79482f24dc77bd4e982f7831daff6ed5fa0b37
Jake Boeckerman <jakebman@gmail.com>
2019-12-10 16:20:12 -0700

    Second star

---
+++ 1/second.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+def intInput():
+    return int(input())
+
+def fuelFor(mass):
+    return max(0, mass // 3 - 2)
+
+def rocketEquation(mass):
+    if mass < 0: return 0
+    sum = 0
+    while mass > 0:
+        mass = fuelFor(mass)
+        sum += mass
+        #wrong, because it counts the original mass: sum, mass = sum + mass, fuelFor(mass)
+        print (mass, "+", end=" ")
+    print ("=", sum)
+    return sum
+
+
+def main():
+    total = 0
+    while True:
+        print(total)
+        i = rocketEquation(intInput())
+        total+=i;
+
+if __name__ == "__main__":
+    main()
