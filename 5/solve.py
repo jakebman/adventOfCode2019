69e33400641a6dab674337ba2361e2801c8386fe
Jake Boeckerman <jakebman@gmail.com>
2019-12-11 21:31:32 -0700

    day 5 (trivial IO) code

--- 5/solve.py
+++ 5/solve.py
@@ -1,6 +1,10 @@
-
+from IntCode import IntCode
 
 def main():
+    raw_program = open("input.txt").readline()
+    program = list(map(int, raw_program.split(",")))
+    p = IntCode(program)
+    p.run()
 
 
 if __name__ == "__main__":
