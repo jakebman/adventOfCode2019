84a05b2496c419c2a0b9827ca5c2625e1fb028b4
Jake Boeckerman <jakebman@gmail.com>
2019-12-13 22:49:39 -0700

    day 11 star 1

--- 11/solve.py
+++ 11/solve.py
@@ -1,62 +1,81 @@
 from itertools import permutations
+from pprint import pprint
 from threading import Thread
-from time import sleep
+from collections import defaultdict, namedtuple
 
 from IntCode import IntCode, setDebugStream
 
-from queue import Queue
+from queue import Queue, Empty
+
+point = namedtuple("point", "row column")
+UP = point(-1, 0)
+DOWN = point(1, 0)
+LEFT = point(0, -1)
+RIGHT = point(0, 1)
+CLOCKWISE = {
+    UP: RIGHT,
+    RIGHT: DOWN,
+    DOWN: LEFT,
+    LEFT: UP,
+}
+COUNTERCLOCKWISE = {
+    UP: LEFT,
+    LEFT: DOWN,
+    DOWN: RIGHT,
+    RIGHT: UP,
+}
+
+
+def debugPrinter(queue):
+    while True:
+        print(queue.get())
 
 
 def main():
     raw_program = open("input.txt").readline()
     program = list(map(int, raw_program.split(",")))
 
-    values = {}
-    for phases in permutations(range(5,10)):
-        value = spawnThreads(phases, program)
-        print(phases, "got", value)
-        values[phases] = value
-    sleep(5)
-    second = lambda x: x[1]
-    l = list(values.items())
-    l.sort(key=second)
-
-    for item in l:
-        print(item)
-    best = max(values.items(), key=second)
-    print(best)
-
-def debugPrinter(queue):
-    while True:
-        print(queue.get())
-
-def spawnThreads(phases, program):
-    queues = [Queue() for _ in phases]
-    queues.append(queues[0])  # for wraparound
-    names = [chr(ord("A") + i) for i in range(len(phases))]
-
     debug = Queue()
     setDebugStream(lambda *args: debug.put(args))
     Thread(name="Debug Printer", target=debugPrinter, args=[debug]).start()
-    threads = []
-    for phase, input, output, name in zip(phases, queues, queues[1:], names):
-        input.put(phase)
 
-        a = IntCode(program, input=input.get, output=output.put, name=name)
-        thread = Thread(name=name, target=a.run)
-        threads.append(thread)
-        thread.start()
-        print("Spawned", name, "with", phase)
-    # initial input
-    queues[0].put(0)
+    queries = Queue()
+    commands = Queue()
+    a = IntCode(program, name="control", input=queries.get, output=commands.put)
+    control = a.run_as_thread()
+    field = {}
+    Thread(name="hull painter", target=hullPainter, args=(queries, commands, field)).start()
+    control.join()
+    pprint(field)
+    print(len(field))
 
-    for thread in threads:
-        print("joining", thread.getName())
-        thread.join()
 
-    got = queues[0].get()
-    print("Joined all for perm", phases, "and got", got)
-    return got
+def query(p, field):
+    if p in field:
+        return field[p]
+    return 0
+
+
+def hullPainter(queries, commands, field):
+    position = point(0, 0)
+    direction = UP
+    while True:
+        queries.put(query(position, field))
+        try:
+            paint = commands.get(timeout=3)
+            turnRight = commands.get(timeout=3)
+            field[position] = paint
+            if turnRight:
+                direction = CLOCKWISE[direction]
+            else:
+                direction = COUNTERCLOCKWISE[direction]
+            position = point(position.row + direction.row, position.column + direction.column)
+        except Empty:
+            print("Done painting")
+            break
+
+    pprint(field)
+    print(len(field))
 
 
 if __name__ == "__main__":
